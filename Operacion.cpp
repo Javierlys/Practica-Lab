6204c3747e3713582d16cdfa5f458aad832ad8d7
Javierlys Almeida <109442512+Javierlys@users.noreply.github.com>
2025-02-07 19:28:01 -0400

    Operacion suma

---
+++ Operacion.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+int main() {
+    //Declaración de las variables
+    float x= 53;
+    float y= 0.005;
+    float z= -27;
+    float suma; 
+    //Realizar la suma
+    suma= x+y+z;
+    //Mostrar el resultado 
+
+    std::cout<<suma;
+    
+    return 0;
+}
